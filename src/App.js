0748e2274079b3c09fb3c883924f1d4f359a281e
Víctor Illanes <villanes9@gmail.com>
2022-01-31 02:28:39 -0300

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+
+
+function App(){
+
+      return (
+      <BrowserRouter>
+            <h1>HELLO WORLD</h1>
+      </BrowserRouter>
+      );
+
+
+}
+
+
+export default App;
